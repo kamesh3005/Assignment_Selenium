6ecf9abb1486e281462aca381605bdab512bba79
kamesh.p <charukamesh1430@gmail.com>
2022-09-26 20:21:57 +0530

    second commite

--- src/test/java/com/project/SignupPage.java
+++ src/test/java/com/project/SignupPage.java
@@ -1,9 +1,31 @@
 package com.project;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SignupPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\kamesh p\\eclipse-workspace\\MySelenium\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.demoblaze.com/index.html");
+
+		driver.manage().window().maximize();
+		driver.findElement(By.id("signin2")).click();
+		driver.findElement(By.id("sign-username")).sendKeys("niranjan.kam11@gmail.com");
+		driver.findElement(By.id("sign-password")).sendKeys("Kamesh@30");
+		List<WebElement> butt = driver.findElements(By.tagName("button"));
+
+		for (WebElement b : butt) {
+			if (b.getText().equals("Sign up")) {
+				b.click();
+			}
+		}
 
 	}
 
